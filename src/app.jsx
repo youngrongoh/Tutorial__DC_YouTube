2fee2683392b63a9c74f24b03abec740d3d33bca
YoungRong.oh <youngrong.oh@gmail.com>
2020-11-10 23:36:27 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import VideoList from './components/video_list/video_list';
 import './app.css';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=bts&key=AIzaSyAxsnLP-oBAqqnF1G1F4TL5wiM9Q2hrJYc", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+    return(
+      <VideoList videos={videos} />
+    );
 }
 
 export default App;
