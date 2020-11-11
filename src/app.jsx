db9a4dcf18f987bae75e14b74da1fc37359084b9
YoungRong.oh <youngrong.oh@gmail.com>
2020-11-11 17:39:03 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import VideoList from './components/video_list/video_list';
-import './app.css';
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "./app.css";
 
 function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+      method: "GET",
+      redirect: "follow",
     };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=bts&key=AIzaSyAxsnLP-oBAqqnF1G1F4TL5wiM9Q2hrJYc", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAxsnLP-oBAqqnF1G1F4TL5wiM9Q2hrJYc",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
   }, []);
 
-    return(
+  return (
+    <>
+      <header className="header">
+        <a href="/" className="logo">
+          <div className="logoImg">
+            <i className="fab fa-youtube"></i>
+          </div>
+          <h1 className="logoTitle">Youtube</h1>
+        </a>
+        <form className="search">
+          <input type="text" className="searchInput" placeholder="Search.." />
+          <button type="submit" className="searchBtn">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
+      </header>
       <VideoList videos={videos} />
-    );
+    </>
+  );
 }
 
 export default App;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 const VideoList = (props) => (
-      <ul>
-        {props.videos.map(video => (
-          <VideoItem key={video.etag} video={video} />
-        ))}
-      </ul>
-  );
+  <ul className={styles.videos}>
+    {props.videos.map((video) => (
+      <VideoItem key={video.etag} video={video} />
+    ))}
+  </ul>
+);
 
 export default VideoList;
